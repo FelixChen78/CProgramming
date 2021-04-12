8c3e4919a06d9f3772fc8afc5627b9f07288855a
FelixChen78 <fchen33@buffalo.edu>
2021-04-12 05:48:52 -0400

    v3.3

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define LOWER 0 /* Lower limit of table */
 #define UPPER 300 /* upper limit */
 #define STEP 20 /* step size */
 
-void printHelloWorld()
+void printHelloWorld() // Ex 1-1
 {
     char helloWorld[] = "Hello, World ! \n";
     printf("%s", helloWorld);
+
 }
 
+void unknownEscapeSequence() // Ex 1-2
+{
+    printf("\c"); /* Ignores backslash because \c is unknown*/
+}
 /** print Fahrenheit-Celsius table
         for fahrenheit = 0, 20, ..., 300 */
 void fahrenheitCelsiusTable()
@@ -28,20 +32,25 @@ void fahrenheitCelsiusTable()
         fahrenheit = fahrenheit + step;
     }
 }
-void fahrenheitCelsiusReverse()
-{
-    float fahrenheit, celsius;
-    printf("__________________________ \n");
-    printf("|Fahrenheit-Celsius Table| \n");
-    printf("-------------------------- \n");
 
-    for (fahrenheit = UPPER; fahrenheit > LOWER; fahrenheit -= STEP) {
-        celsius = 5 * (fahrenheit - 32) / 9;
-        printf("|\t%3.0f \t| \t%6.1f\t |\n", fahrenheit, celsius);
+void celsiusFahrenheitTable() //Ex 1-3
+{
+    int fahrenheit, celsius;
+    int lower, upper, step;
+    lower = -20.0;
+    upper = 150.0;
+    step = 5.0;
+    celsius = lower;
+
+    while(celsius <= upper) {
+        fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+        printf("%d\t%d\n", celsius, fahrenheit);
+        celsius = celsius + step;
     }
-    printf("-------------------------- \n");
+
 }
-void fahrenheitCelsiusTableFloat()
+
+void fahrenheitCelsiusTableFloat() // Ex 1-4
 {
     float fahrenheit, celsius;
     float lower, upper, step;
@@ -62,6 +71,20 @@ void fahrenheitCelsiusTableFloat()
 
 }
 
+void fahrenheitCelsiusReverse() // Ex 1-5
+{
+    float fahrenheit, celsius;
+    printf("__________________________ \n");
+    printf("|Fahrenheit-Celsius Table| \n");
+    printf("-------------------------- \n");
+
+    for (fahrenheit = UPPER; fahrenheit > LOWER; fahrenheit -= STEP) {
+        celsius = 5 * (fahrenheit - 32) / 9;
+        printf("|\t%3.0f \t| \t%6.1f\t |\n", fahrenheit, celsius);
+    }
+    printf("-------------------------- \n");
+}
+
 void inputOutput()
 {
     int c;
@@ -80,134 +103,36 @@ void inputOutput2()
         putchar(c);
 }
 
-void eof()
+void eof() // Ex 1-6
 {
     /**TO SIMULATE EOF:
      *  On mac use: ctrl + d
      *  On windows use: ctrl + z
     */
-    printf("Value of EOF = %d", EOF); /* prints value of EOF */
     printf("\nEnter char to output 0 or ctrl + d to output 1 ");
     printf("\ngetchar() != EOF: %i", getchar() != EOF); /* prints 1 when not EOF. prints 0 when EOF*/
 }
+void eof2() //Ex 1-7
+{
+    printf("Value of EOF = %d", EOF); /* prints value of EOF */
+}
+
+
 int main()
 {
 //    printHelloWorld();
+//    unknownEscapeSequence();
 //    fahrenheitCelsiusTable();
+//    celsiusFahrenheitTable();
 //    fahrenheitCelsiusTableFloat();
 //    fahrenheitCelsiusReverse();
 //    inputOutput();
 //    inputOutput2();
-    eof();
+//    eof();
+
 
 
     return 0;
 }
 
 
-/**                            CHEAT CHEAT                                */
-
-
-/**                            C PROGRAMMING                              */
-
-/** C STRINGS
- * new line = \n
- * tab = \t
- * backslash = \\
- * double quote = \"
- * string format specifier = %s
- * character format specifier = %c
- * integer format specifier = %d or %i
- * float format specifier = %f
- * width and precision = eg. %6.2f a float where it is 6 character wide
- * and 2 characters wide after decimal point
- */
-
-/** COMPILE AND RUN C PROGRAM
- * compile = cc fileName.c
- * run = a.out
- * run in current working directory = ./a.out
- */
-
-/** CODE NAME CONVENTION
- * CLASSES: PASCAL CASE = ClassName
- * METHODS: CAMEL CASE = methodName
- * VARIABLES: CAMEL CASE = variableName
- */
-
-
-
-/**                              GIT/GITHUB                                   */
-
-/** CLONE A EXISTING PROJECT FROM GITHUB
- * git clone <repository link>
- */
-
-/** ADD PROJECT TO GITHUB
- * Initial commit and push to repository:
- * git init
- * git add .
- * git commit -m "first commit"
- * git branch -m Main
- * git remote add origin https://github.com/FelixChen78/MyRepository.git
- * git push -u origin main
- */
-
-/** FORK REPOSITORY AND PULL REQUEST
- * Fork a GitHub repository.
- * Clone the forked repository to your local system.
- * Add a Git remote for the original repository.
- * Create a feature branch in which to place your changes.
- * Make your changes to the new branch.
- * Commit the changes to the branch.
- * Push the branch to GitHub.
- * Open a pull request from the new branch to the original repo.
- * Clean up after your pull request is merged.
- */
-
-
-/** CREATE NEW BRANCH FROM MASTER BRANCH
- * git checkout main
- * git branch <new branch>
- * git checkout <new branch>
- */
-
-/** ADD CHANGES TO BRANCH
- * git add . or git add -A
- * git commit -m "commit message"
- * git push -u origin <local-branch>
- */
-
-/** MERGE CHANGES TO MASTER BRANCH
- * git add . or git add -A
- * git commit -m "commit message"
- * git checkout main
- * git merge <new branch>
- */
-
-/** GIT COMMANDS
- * CREATE NEW BRANCH:
- *  git branch -m <local-branch>
- * ADD GITHUB REPOSITORY TO BRANCH:
- *  git remote add origin <github repository link>
- * PUSH COMMIT TO BRANCH:
- *  git push -u origin <local-branch>
- * DELETE BRANCH:
- *  git branch -d <local-branch>
- * ADD CHANGES TO GIT:
- *  git add .
- * COMMIT FILES TO GIT:
- *  git commit -m "message"
- * PUSH COMMIT TO BRANCH:
- *  git push -u origin <local-branch>
- * SWITCH BRANCH:
- *  git checkout <local-branch>
- * MERGE BRANCH INTO CURRENT BRANCH:
- *  git merge <local-branch>
- * CHECK USER COMMIT LOG:
- *  git log --graph
- * CREATE AND SWITCH TO NEW BRANCH:
- *  git checkout -b <local-branch>
- * CLONE REPOSITORY FROM GITHUB:
- *  git clone <repository link>
- */
