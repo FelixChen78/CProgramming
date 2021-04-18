38e98eb240228be3e6796df1d5ba6ec2cb7d9b66
FelixChen78 <fchen33@buffalo.edu>
2021-04-18 09:54:55 -0400

    refractor directory

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/felixchen/CLionProjects/CProgramming
+# Install script for directory: /Users/felixchen/CLionProjects/c-practice
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/felixchen/CLionProjects/CProgramming/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/felixchen/CLionProjects/c-practice/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
