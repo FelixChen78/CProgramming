b61c24795641159333be9e8d5d6b5421bfc3757a
FelixChen78 <fchen33@buffalo.edu>
2021-05-14 17:50:11 -0400

    cmake

--- src/chapter2/main.c
+++ src/chapter2/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define true 1
+#define false 0
+
 void rangeOfData() //Ex 2-1
 {
     printf("CHAR_MIN = %d, CHAR_MAX = %d \n", CHAR_MIN, CHAR_MAX);
@@ -12,8 +15,9 @@ void rangeOfData() //Ex 2-1
     printf("UINT_MIN = %d, UINT_MAX = %d \n", 0, (unsigned int)UINT_MAX);
     printf("ULONG_MIN = %d, ULONG_MAX = %lu \n", 0, (unsigned long) ULONG_MAX);
     printf("USHRT_MIN = %d, USHRT_MAX = %d \n", 0, (unsigned short) USHRT_MAX);
-
 }
+
+
 /** Driver Code */
 int main()
 {
