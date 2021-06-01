e98f69f014c6638b4fd755e1916b40e9dd4fbb23
FelixChen78 <fchen33@buffalo.edu>
2021-05-31 23:52:51 -0400

    rm

--- src/chapter2/main.c
+++ src/chapter2/main.c
@@ -1,52 +1,12 @@
 #include <stdio.h>
 #include <limits.h>
 
-#define true 1
-#define false 0
-
-void rangeOfData() //Ex 2-1
-{
-    printf("CHAR_MIN = %d, CHAR_MAX = %d \n", CHAR_MIN, CHAR_MAX);
-    printf("INT_MIN = %d, INT_MAX = %d \n", INT_MIN, INT_MAX);
-    printf("LONG_MIN = %ld, LONG_MAX = %ld \n", (long) LONG_MIN, (long) LONG_MAX);
-    printf("SCHAR_MIN = %d, SCHAR_MAX = %d \n", SCHAR_MIN, SCHAR_MAX);
-    printf("SHRT_MIN = %d, SHRT_MAX = %d \n", SHRT_MIN, SHRT_MAX);
-    printf("UCHAR_MIN = %d, UCHAR_MAX = %d \n", 0, UCHAR_MAX);
-    printf("UINT_MIN = %d, UINT_MAX = %d \n", 0, (unsigned int)UINT_MAX);
-    printf("ULONG_MIN = %d, ULONG_MAX = %lu \n", 0, (unsigned long) ULONG_MAX);
-    printf("USHRT_MIN = %d, USHRT_MAX = %d \n", 0, (unsigned short) USHRT_MAX);
-}
-
-void noRelational() //Ex 2-2
-{
-    int c, i = 0, lim = 10;
-    char s[lim];
-
-    while ((c=getchar()) != EOF) {
-        if (c != '\n') {
-            if (i < lim - 1) {
-                s[i] = c;
-                i++;
-            }
-            else {
-                break;
-            }
-        }
-        else {
-            break;
-        }
-    }
-    printf("%s \n", s);
-}
-
 
 
 
 /** Driver Code */
 int main()
 {
-//    rangeOfData();
-//    noRelational();
     return 0;
 }
 
